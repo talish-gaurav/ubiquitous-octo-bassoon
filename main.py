6f7b9a3b7471aea2f61ce348e17b2ca7f5704f06
talish-gaurav <97028189+talish-gaurav@users.noreply.github.com>
2024-12-17 18:52:49 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import math
+
+print("square root of 16 is", math.sqrt(16))
+print("factorial of 5 is ", math.factorial(5))
+print(" 2 to the power of 4 is", math.pow(2,4))
+print("pi value is",math.pi)
+print("cos value is",math.cos(22))
+print("sin value is",math.sin(22))
+print("tan value is",math.tan(22))
